efaccec154b857c1e632996b5b8f713f77a0e49c
Yannick Fricke <yfr@mts.de>
2020-04-20 16:37:24 +0200

    Added the logger to the application

--- src/Application.ts
+++ src/Application.ts
@@ -1,7 +1,16 @@
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
+import { Logger } from 'winston';
+import { Services } from './ioc/Services';
 
 @injectable()
 export class Application {
+    private logger: Logger;
+
+    constructor(@inject(Services.Logger) logger: Logger) {
+        this.logger = logger;
+    }
+
     async run() {
+        this.logger.info('Running the RFC synchronizer');
     }
 }
