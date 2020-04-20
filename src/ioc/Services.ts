4758ba72fceda47f6041dd5ce9b7cc554568da32
Yannick Fricke <yfr@mts.de>
2020-04-20 17:28:47 +0200

    Bind the Github token to the container

--- src/ioc/Services.ts
+++ src/ioc/Services.ts
@@ -1,3 +1,8 @@
 export const Services = {
     Logger: Symbol.for('System.Logger'),
+    Net: {
+        Github: {
+            Token: Symbol.for('System.Net.Github.Token'),
+        },
+    },
 };
--- src/ioc/module/NetworkContainerModule.ts
+++ src/ioc/module/NetworkContainerModule.ts
@@ -1,9 +1,13 @@
 import { ContainerModule } from 'inversify';
 import { IssueSynchronizer } from '../../net/IssueSynchronizer';
+import { Services } from '../Services';
 
 export class NetworkContainerModule extends ContainerModule {
     constructor() {
         super((bind) => {
+            // Bind the Github token to the container
+            bind(Services.Net.Github.Token).toConstantValue(process.env.GH_TOKEN ?? '');
+
             bind(IssueSynchronizer).toSelf().inSingletonScope();
         });
     }
