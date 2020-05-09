a87efd3ba4e9a38b875776cf2350fbd17581101b
Yannick Fricke <yannickfricke@googlemail.com>
2020-05-10 01:34:25 +0200

    Load the database container module

--- src/ioc/container.ts
+++ src/ioc/container.ts
@@ -2,6 +2,7 @@ import { Container } from 'inversify';
 import { ApplicationContainerModule } from './module/ApplicationContainerModule';
 import { LoggerContainerModule } from './module/LoggerContainerModule';
 import { NetworkContainerModule } from './module/NetworkContainerModule';
+import { DatabaseContainerModule } from './module/DatabaseContainerModule';
 
 export const getConfiguredContainer = async (): Promise<Container> => {
     const container = new Container();
@@ -9,6 +10,7 @@ export const getConfiguredContainer = async (): Promise<Container> => {
     container.load(new ApplicationContainerModule());
     container.load(new LoggerContainerModule());
     container.load(new NetworkContainerModule());
+    await container.loadAsync(new DatabaseContainerModule());
 
     return container;
 };
